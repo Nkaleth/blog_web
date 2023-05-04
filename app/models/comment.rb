e5f8c5ec3280ad00b20a3de2e0fff76043b3d8d7
Nkaleth <nseguralu@gmail.com>
2023-05-03 19:37:58 -0500

    FIX method update comment in order to count comments for a posts not for users

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_create :increment_author_comments_count
+
+  private
+
+  def increment_author_comments_count
+    post.increment!(:comments_count)
+  end
 end
