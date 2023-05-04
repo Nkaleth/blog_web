43cff7abfbdcac13f7b641dabcea197a4f1e51d7
Nkaleth <nseguralu@gmail.com>
2023-05-03 19:32:26 -0500

    ADD methods for last 5 comments and posts couns for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+
+  after_create :increment_author_posts_count
+
+  def five_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def increment_author_posts_count
+    author.increment!(:posts_count)
+  end
 end
