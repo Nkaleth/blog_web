5b00bdc9e99bcca233348735afe1100fca4ec16c
Nkaleth <nseguralu@gmail.com>
2023-05-04 17:18:23 -0500

    ADD validation for title of posts to not be blank

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
   belongs_to :author, class_name: 'User'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
