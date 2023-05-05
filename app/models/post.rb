90347b3bc91f9455572cea8f2e353013e258ba9d
Nkaleth <nseguralu@gmail.com>
2023-05-04 20:19:36 -0500

    ADD test for post model and refactor users, post migrate files in order to pass the tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   belongs_to :author, class_name: 'User'
-  has_many :comments, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'post_id'
+  has_many :likes, foreign_key: 'post_id'
 
   after_create :increment_author_posts_count
 
