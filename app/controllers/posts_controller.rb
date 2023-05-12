04e2ca6ea3be96fa345d97288433bc9946d1a1d8
Nkaleth <nseguralu@gmail.com>
2023-05-11 23:41:05 -0500

    ADD feature create new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,23 @@ class PostsController < ApplicationController
 
   def new
     @first_user = current_user
-    @posts = Post.new
+    @post = Post.new
+  end
+
+  def create
+    @first_user = current_user
+    @post = Post.new(post_params.merge(author: @first_user))
+
+    if @post.save
+      redirect_to user_post_path(@first_user, @post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show, :new] do
-    resources :posts, only: [:index, :show, :new, create:]
+    resources :posts, only: [:index, :show, :new, :create]
   end
   # Defines the root path route ("/")
   root "users#index"
